f3b28d38ee9bf6d36996fccffc35c199a9a915d0
Reidjoss <reidjoss@gmail.com>
2020-02-07 08:19:03 +0100

    teeeeeemp
    temp

--- skjema.php
+++ skjema.php
@@ -44,6 +44,7 @@ if (!isset($_SESSION["authenticated"])) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 $authenticated = false;
 
@@ -76,6 +77,8 @@ try {
 
 // echo " " + $hash + " ";
 
+=======
+>>>>>>> parent of a0820ee... cookiefiks i git
 =======
 >>>>>>> parent of a0820ee... cookiefiks i git
 =======
