a4ab04d4381166755ab9796cf41b7d970f987ff6
Reidjoss <reidjoss@gmail.com>
2020-09-27 20:29:28 +0200

    prøver å fjerne auth.

--- skjema.php
+++ skjema.php
@@ -37,7 +37,7 @@ if (!isset($_SESSION["authenticated"])) {
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="css/master.css">
 </head>
-
+# /* Prøver nok en gang å fjerne autentisering */
 <?php
 $authenticated = false;
 
